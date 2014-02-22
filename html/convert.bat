8fa09e578644e71534e6cb35050e066ca6320737
jiangshui <yujiangshui@gmail.com>
2014-02-22 18:11:48 +0800

    修正 bat

--- html/convert.bat
+++ html/convert.bat
@@ -1,5 +1,5 @@
 @echo off
-echo 根据项目需求，在批处理文件中设置对应的 nested expanded compact compressed 参数。
+
 echo You can edit this file to adapt your project. ( nested expanded compact compressed )
-echo -------------------------------------------------------------------------------------
+
 sass --style compressed --watch style.scss:style.css
